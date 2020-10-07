b92ba8a693b552d27b0022dc740a8899fd2e1f83
Your Name <you@example.com>
2020-10-07 21:37:34 +0530

    nothing

--- frontend/src/App1.js
+++ frontend/src/App1.js
@@ -1,42 +1,43 @@
-import React,{useState,useEffect,createContext} from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import './App.css';
 
-
-
-import Code from "./Components/Code"
-import Output from "./Components/Output"
-import Example from "./Components/Example"
-import Compile from "./Components/Compile"
-
-
+import Code from './Components/Code';
+import Output from './Components/Output';
+import Example from './Components/Example';
+import Compile from './Components/Compile';
 
 function App() {
   const FetchData = createContext(null);
-  
-  
-  const [examples,setExamples] = useState("examples");
-  const [outputs,setOutputs] = useState("outputs");
-  const [error,setErrors] = useState("errors");
 
-  const updateErrors = (errors) => {setErrors(errors)}
-  const updateExamples = (examples) => {setExamples(examples)}
-  const updateOutputs = (out) => {setOutputs(out)}
+  const [examples, setExamples] = useState('examples');
+  const [outputs, setOutputs] = useState('outputs');
+  const [error, setErrors] = useState('errors');
 
-  const api = {updateErrors,updateExamples,updateOutputs}
+  const updateErrors = (errors) => {
+    setErrors(errors);
+  };
+  const updateExamples = (examples) => {
+    setExamples(examples);
+  };
+  const updateOutputs = (out) => {
+    setOutputs(out);
+  };
+
+  const api = { updateErrors, updateExamples, updateOutputs };
   const clickRun = () => {
-      console.log("run")
-      setErrors("1");
-      setExamples("2");
-      setOutputs("3");
-      console.log(outputs);
-  }
+    console.log('run');
+    setErrors('1');
+    setExamples('2');
+    setOutputs('3');
+    console.log(outputs);
+  };
 
   return (
-    <div className="App" id="content">
-        <Code clickRun={clickRun}/>
-        <Output outputs={outputs}/>
-        {/* <Example examples={examples}/> */}
-        <Compile error={error}/>
+    <div className='App' id='content'>
+      <Code clickRun={clickRun} />
+      <Output outputs={outputs} />
+      {/* <Example examples={examples}/> */}
+      <Compile error={error} />
     </div>
   );
 }
